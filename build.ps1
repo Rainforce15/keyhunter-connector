ffe178c99c9c97aef898b0ade8ed1e138e91ff82
rainforce15 <rainforce15@gmail.com>
2023-07-17 06:59:40 +0200

    updated build script

--- build.ps1
+++ build.ps1
@@ -2,4 +2,4 @@ If(!(test-path -PathType container dist))
 {
       New-Item -ItemType Directory -Path dist
 }
-Get-ChildItem -Path src\connector.lua, lib\socket.dll | Compress-Archive -Force -DestinationPath dist\keyhunter-connector.zip
+Get-ChildItem -Path src\*, lib\* | Compress-Archive -Force -DestinationPath dist\keyhunter-connector.zip
